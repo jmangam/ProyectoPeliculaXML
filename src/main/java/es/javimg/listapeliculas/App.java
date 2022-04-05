6eb1ac5e70104cc87981d412842a2de0937380d7
jmangam <jmangam010@g.educaand.es>
2022-04-05 11:00:11 +0200

    Dandole formato a la ventana

--- src/main/java/es/javimg/listapeliculas/App.java
+++ src/main/java/es/javimg/listapeliculas/App.java
@@ -10,37 +10,46 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 
+
 public class App extends Application {
 
+    int cuenta = 0;
+    
     @Override
     public void start(Stage stage) {
         VBox root = new VBox();
         root.setAlignment(Pos.CENTER);
-        root.setSpacing(30);
+        root.setSpacing(10);
         var scene = new Scene(root, 640, 480);
         stage.setScene(scene);
         stage.show();
         
-        Pelicula pelicula1 = new Pelicula("Ficheros en Java");
-        //System.out.println(pelicula1.getTitulo());
-        pelicula1.setTitulo("Listas en Java");
-        //System.out.println(pelicula1.getTitulo());
+        
+        Pelicula pelicula1 = new Pelicula();
+        pelicula1.setTitulo("Michael Jackson, su historia");
+        pelicula1.setDirector("Jeremias Manzano Perez");
+        pelicula1.setProductora("Netflix");
+        pelicula1.setPrecioEntrada(8.5);
         
         Pelicula pelicula2 = new Pelicula();
-        Pelicula pelicula3 = new Pelicula("XML en java");
-        pelicula3.setTitulo("XML en Java 2");
-  
-//        int[] listaNum = new int[10];
-//        Libro[] listaLibros = new Libro[10];
-    
+        pelicula2.setTitulo("El dia de Cristiano Ronaldo");
+        pelicula2.setDirector("Florentino Duran Manchego");
+        pelicula2.setProductora("HBO");
+        pelicula2.setPrecioEntrada(10.90);
+        
+        Pelicula pelicula3 = new Pelicula();
+        pelicula3.setTitulo("Como se creo Apple");
+        pelicula3.setDirector("Sergio Ortelano Salguero");
+        pelicula3.setProductora("Amazon Prime Video");
+        pelicula3.setPrecioEntrada(7.90);
+        
+        
         Peliculas peliculas = new Peliculas();
         peliculas.getListaPelicula().add(pelicula1);
         peliculas.getListaPelicula().add(pelicula2);
         peliculas.getListaPelicula().add(pelicula3);
         
-        
-        
-        
+       
         Button buttonSelectFile = new Button("Guardar XML");
         buttonSelectFile.setOnAction((t) -> {
             UtilXML.guardarDatosXML(stage, peliculas);
@@ -52,45 +61,51 @@ public class App extends Application {
         buttonImportFile.setOnAction((t) -> {
             Peliculas peliculasImport = UtilXML.convertirDatosXML(stage);
             peliculas.añadirPeliculas(peliculasImport);
-            System.out.println(peliculas.getListaPelicula().size());
-            
-            
+            System.out.println(peliculas.getListaPelicula().get(4));
         });
         
-        root.getChildren().addAll(buttonImportFile,buttonSelectFile);
+        root.getChildren().addAll(buttonImportFile,buttonSelectFile);  
         
-        peliculas.añadirPeliculas(peliculas);
+        VBox vbox = new VBox();
+        Label label = new Label();
+        Label label1 = new Label();
+        Label label2 = new Label();
+        Label label3 = new Label();
+        Label label4 = new Label();
         
-        Label label = new Label("a label");
-        root.getChildren().addAll(label);
+        vbox.setStyle("-fx-background-color:#D5D8DC");
+        vbox.getChildren().addAll(label, label1, label2, label3);
+        vbox.setAlignment(Pos.CENTER);
+        vbox.setMaxSize(250,450);
+        
+        root.getChildren().add(vbox);
+           label.setText("Nombre:" + peliculas.getListaPelicula().get(0).getTitulo());
+           label1.setText("Director: " + peliculas.getListaPelicula().get(0).getDirector());
+           label2.setText("Productora: " + peliculas.getListaPelicula().get(0).getProductora());
+           label3.setText("Precio Entrada: " + peliculas.getListaPelicula().get(0).getPrecioEntrada()); 
+//        
         
         Button buttonAvanzar = new Button("Avanzar");
-//        buttonImportFile.setOnAction((t) -> {
-//            
-//        }
+        buttonAvanzar.setOnAction((t) -> {
+            cuenta++;
+            label.setText("Nombre:" + peliculas.getListaPelicula().get(cuenta).getTitulo());
+            label1.setText("Director: " + peliculas.getListaPelicula().get(cuenta).getDirector());
+            label2.setText("Productora: " + peliculas.getListaPelicula().get(cuenta).getProductora());
+            label3.setText("Precio Entrada: " + peliculas.getListaPelicula().get(cuenta).getPrecioEntrada());
+        });
+
 
         Button buttonRetroceder = new Button("Retroceder");
-//        buttonImportFile.setOnAction((t) -> {
-//            
-//        }
+        buttonRetroceder.setOnAction((t) -> {
+            cuenta--;
+            label.setText("Nombre:" + peliculas.getListaPelicula().get(cuenta).getTitulo());
+            label1.setText("Director: " + peliculas.getListaPelicula().get(cuenta).getDirector());
+            label2.setText("Productora: " + peliculas.getListaPelicula().get(cuenta).getProductora());
+            label3.setText("Precio Entrada: " + peliculas.getListaPelicula().get(cuenta).getPrecioEntrada()); 
+            
+        });
 
         root.getChildren().addAll(buttonAvanzar,buttonRetroceder);
-        //mostrar titulo de segundo libro
-//       
-//        System.out.println(listaLibros.get(2).getTitulo());
-//        
-//        //eliminar el segundo libro
-//        listaLibros.remove(1);
-//        
-//        // Mostrar todos los libros de la lista
-//        for(int i=0; i<listaLibros.size();i++) {
-//            System.out.println(listaLibros.get(i).getTitulo());
-//        }
-//        
-//        for(int i=0; i<listaLibros.size();i++) {
-//            System.out.println(listaLibros.get(i));
-//        }
-//      
 
     }
 
